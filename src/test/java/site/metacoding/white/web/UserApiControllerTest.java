cc6485196f19b4fdec39c13a9c4d6c5cd18268a4
seong9566 <dlgustjd9566@gmail.com>
2022-10-29 14:59:40 +0900

    user test완료

--- src/test/java/site/metacoding/white/web/UserApiControllerTest.java
+++ src/test/java/site/metacoding/white/web/UserApiControllerTest.java
@@ -1,53 +1,102 @@
 package site.metacoding.white.web;
 
-import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
+import org.springframework.mock.web.MockHttpSession;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.transaction.annotation.Transactional;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.jayway.jsonpath.DocumentContext;
-import com.jayway.jsonpath.JsonPath;
 
+import site.metacoding.white.domain.Board;
+import site.metacoding.white.domain.BoardRepository;
+import site.metacoding.white.domain.Comment;
+import site.metacoding.white.domain.CommentRepository;
+import site.metacoding.white.domain.User;
+import site.metacoding.white.domain.UserRepository;
+import site.metacoding.white.dto.SessionUser;
 import site.metacoding.white.dto.UserReqDto.JoinReqDto;
 import site.metacoding.white.dto.UserReqDto.LoginReqDto;
-import site.metacoding.white.service.UserService;
+import site.metacoding.white.dto.UserReqDto.UpdateReqDto;
+import site.metacoding.white.util.SHA256;
 
 @ActiveProfiles("test")
-// @Transactional // 통합테스트에서 RANDOM_PORT를 사용하면 새로운 스레드로 돌기 때문에 rollback 무의미
-@Sql("classpath:truncate.sql") // 기본 정책 (전 - 후)
-@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@Sql("classpath:truncate.sql")
+@Transactional // 트랜잭션 안붙이면 영속성 컨텍스트에서 DB로 flush 안됨 (Hibernate 사용시)
+@AutoConfigureMockMvc // MockMvc Ioc 컨테이너에 등록
+@SpringBootTest(webEnvironment = WebEnvironment.MOCK) // 가짜 환경으로 실행
 public class UserApiControllerTest {
 
+    private static final String APPLICATION_JSON = "application/json; charset=utf-8";
+
     @Autowired
-    private TestRestTemplate rt;
+    private MockMvc mvc;
+
     @Autowired
     private ObjectMapper om;
+
     @Autowired
-    private UserService userService;
+    private UserRepository userRepository;
 
-    private static HttpHeaders headers;
+    @Autowired
+    private BoardRepository boardRepository;
 
-    @BeforeAll
-    public static void init() {
-        headers = new HttpHeaders(); // http 요청 header에 필요
-        headers.setContentType(MediaType.APPLICATION_JSON);
+    @Autowired
+    private CommentRepository commentRepository;
+
+    @Autowired
+    private SHA256 sha256;
+
+    private MockHttpSession session;
+
+    @BeforeEach
+    public void sessionInit() {
+        session = new MockHttpSession();
+        User user = User.builder().id(1L).username("ssar").build();
+        session.setAttribute("sessionUser", new SessionUser(user));
     }
 
-    // @Order(1)
+    @BeforeEach
+    public void dataInit() {
+        String encPassword = sha256.encrypt("1234");
+        User user = User.builder().username("ssar").password(encPassword).build();
+        User userPS = userRepository.save(user);
+
+        Board board = Board.builder()
+                .title("스프링1강")
+                .content("트랜잭션관리")
+                .user(userPS)
+                .build();
+        Board boardPS = boardRepository.save(board);
+
+        Comment comment1 = Comment.builder()
+                .content("내용좋아요")
+                .board(boardPS)
+                .user(userPS)
+                .build();
+
+        Comment comment2 = Comment.builder()
+                .content("내용싫어요")
+                .board(boardPS)
+                .user(userPS)
+                .build();
+
+        commentRepository.save(comment1);
+        commentRepository.save(comment2);
+    }
+
     @Test
-    public void join_test() throws JsonProcessingException {
+    public void join_test() throws Exception {
         // given
         JoinReqDto joinReqDto = new JoinReqDto();
         joinReqDto.setUsername("very");
@@ -57,50 +106,73 @@ public class UserApiControllerTest {
         System.out.println(body);
 
         // when
-        HttpEntity<String> request = new HttpEntity<>(body, headers);
-        ResponseEntity<String> response = rt.exchange("/join", HttpMethod.POST,
-                request, String.class);
-
+        ResultActions resultActions = mvc
+                .perform(MockMvcRequestBuilders.post("/join").content(body)
+                        .contentType(APPLICATION_JSON).accept(APPLICATION_JSON)
+                        .session(session));
         // then
-        // System.out.println(response.getStatusCode());
-        // System.out.println(response.getBody());
-
-        System.out.println("디버그 : " + response.getStatusCodeValue());
-
-        DocumentContext dc = JsonPath.parse(response.getBody());
-        // System.out.println(dc.jsonString());
-        Integer code = dc.read("$.code");
-        Assertions.assertThat(code).isEqualTo(1);
+        MvcResult mvcResult = resultActions.andReturn();
+        System.out.println("디버그 : " + mvcResult.getResponse().getContentAsString());
+        resultActions.andExpect(MockMvcResultMatchers.jsonPath("$.code").value(1L));
+        resultActions.andExpect(MockMvcResultMatchers.jsonPath("$.data.username").value("very"));
     }
 
     @Test
-    public void login_test() throws JsonProcessingException {
-        // data init
-        JoinReqDto joinReqDto = new JoinReqDto();
-        joinReqDto.setUsername("very");
-        joinReqDto.setPassword("1234");
-        userService.save(joinReqDto);
+    public void login_test() throws Exception {
 
         // given
         LoginReqDto loginReqDto = new LoginReqDto();
-        loginReqDto.setUsername("very");
+        loginReqDto.setUsername("ssar");// data init -> ssar임
         loginReqDto.setPassword("1234");
         String body = om.writeValueAsString(loginReqDto);
-        System.out.println("디버그 : " + body);
 
         // when
-        HttpEntity<String> request = new HttpEntity<>(body, headers);
-        ResponseEntity<String> response = rt.exchange("/login", HttpMethod.POST,
-                request, String.class);
-        System.out.println("디버그 : " + response.getBody());
+        ResultActions resultActions = mvc
+                .perform(MockMvcRequestBuilders.post("/login").content(body)
+                        .contentType(APPLICATION_JSON).accept(APPLICATION_JSON)
+                        .session(session));
 
         // then
-        DocumentContext dc = JsonPath.parse(response.getBody());
-        Integer code = dc.read("$.code");
-        String username = dc.read("$.data.username");
-        Assertions.assertThat(code).isEqualTo(1);
-        Assertions.assertThat(username).isEqualTo("very");
-
+        MvcResult mvcResult = resultActions.andReturn();
+        System.out.println("디버그 : " + mvcResult.getResponse().getContentAsString());
+        resultActions.andExpect(MockMvcResultMatchers.jsonPath("$.code").value(1L));
+        resultActions.andExpect(MockMvcResultMatchers.jsonPath("$.data.username").value("ssar"));
     }
 
+    @Test
+    public void findById_test() throws Exception {
+        // given
+        // findById는 given데이터가 없음 대신 session에 있는 id값이 필요함.
+        // when
+        ResultActions resultActions = mvc
+                .perform(MockMvcRequestBuilders.get("/user").accept(APPLICATION_JSON).session(session));// session전달
+
+        // then
+        MvcResult mvcResult = resultActions.andReturn();
+        System.out.println("디버그 : " + mvcResult.getResponse().getContentAsString());
+        resultActions.andExpect(MockMvcResultMatchers.status().isOk());
+        resultActions.andExpect(MockMvcResultMatchers.jsonPath("$.data.username").value("ssar"));
+    }
+
+    @Test
+    public void update_test() throws Exception {
+        // given
+        UpdateReqDto updateReqDto = new UpdateReqDto();
+        updateReqDto.setUsername("very");
+        updateReqDto.setPassword("4567");
+
+        String body = om.writeValueAsString(updateReqDto);
+
+        // when
+        ResultActions resultActions = mvc
+                .perform(MockMvcRequestBuilders.put("/update").content(body)
+                        .contentType(APPLICATION_JSON).accept(APPLICATION_JSON)
+                        .session(session)); // update에 session의 id값이 필요함.
+        // then
+        MvcResult mvcResult = resultActions.andReturn();
+        System.out.println("디버그 : " + mvcResult.getResponse().getContentAsString());
+        resultActions.andExpect(MockMvcResultMatchers.jsonPath("$.code").value(1L));
+        resultActions.andExpect(MockMvcResultMatchers.jsonPath("$.data.username").value("very"));
+        resultActions.andExpect(MockMvcResultMatchers.jsonPath("$.data.password").value("4567"));
+    }
 }
